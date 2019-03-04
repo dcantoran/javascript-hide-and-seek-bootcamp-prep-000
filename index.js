bb04f3b053be418263adc758e49ccb3368a9888e
David Cantoran <davidcantoran36@gmail.com>
2019-03-04 22:13:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  var lista = document.getElementById("app").querySelectorAll()
+  var lista = document.getElementById("app").querySelectorAll("ul .ranked-list li")
 }
