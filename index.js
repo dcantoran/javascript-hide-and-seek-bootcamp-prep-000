f60cf509cdebf6287bc6e6d19ed8e7eec5e168ed
David Cantoran <davidcantoran36@gmail.com>
2019-03-04 22:22:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.getElementById("nested").querySelector(".target");
+}
+
+function increaseRankBy(n) {
+  var lista = document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  for (var i = 0; i < lista.length; i ++) {
+    lista[i].innerHTML = parseInt(lista[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var deep = document.getElementById("grand-node").querySelectorAll("div");
+  
+  for (var i = 0; i < deep.length-1; i ++) {
+    var result = deep[i].querySelector("div");
+    
+  }
+  return result;
+}
+
 
